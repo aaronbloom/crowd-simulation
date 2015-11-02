6721700a56ee2ab3e2a0f965f3cd5586317b451b
aaronbloom <hiaaronbloom@gmail.com>
2015-11-02 18:21:42 +0000

    CS-10 & CS-9: Added Environment manager to control simulation environment bounds. Added Boid manager that can spawn a specified amount of boids within the environment

--- 3D Simulation/Crowd simulation/Assets/Scripts/Boid.cs
+++ 3D Simulation/Crowd simulation/Assets/Scripts/Boid.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Boid : MonoBehaviour {
-
+    
 	// Use this for initialization
 	void Start () {
 	
---
+++ 3D Simulation/Crowd simulation/Assets/Scripts/BoidManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoidManager : MonoBehaviour {
+
+    private EnvironmentManager EnvironmentManager;
+    public int NumberOfBoids = 1;
+
+    private void SpawnBoids () {
+        System.Random random = new System.Random();
+
+        for (int i = 0; i < NumberOfBoids; i++)
+        {
+            Vector3 positionOffset = new Vector3(
+                random.Next(0, (int)EnvironmentManager.Bounds.x),
+                random.Next(0, (int)EnvironmentManager.Bounds.y),
+                random.Next(0, (int)EnvironmentManager.Bounds.z));
+            Instantiate(Resources.Load("Prefabs/CubeBoid"), transform.position + positionOffset, transform.rotation);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        EnvironmentManager = FindObjectOfType(typeof(EnvironmentManager)) as EnvironmentManager;
+        SpawnBoids();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
