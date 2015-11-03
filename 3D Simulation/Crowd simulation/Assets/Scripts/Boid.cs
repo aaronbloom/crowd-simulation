f1ce62bee6fc27658ff21a852ebd406e9333c07a
aaronbloom <hiaaronbloom@gmail.com>
2015-11-03 22:47:03 +0000

    CS-14: Added cylinder boid to give more visual cue to boid direction. Included basic vector field for boid direction.

--- 3D Simulation/Crowd simulation/Assets/Scripts/Boid.cs
+++ 3D Simulation/Crowd simulation/Assets/Scripts/Boid.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 
 public class Boid : MonoBehaviour {
+    private static float speed = 4;
+    public Vector3 direction;
     
 	// Use this for initialization
 	void Start () {
-	
+        direction = Vector3.forward;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position += this.direction * speed;
+    }
 }
