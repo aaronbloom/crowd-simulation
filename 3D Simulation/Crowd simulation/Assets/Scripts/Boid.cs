927093f35b8a13183d46a48b7d3a5e55320c558e
aaronbloom <hiaaronbloom@gmail.com>
2015-11-04 14:03:15 +0000

    CS-14: Added logic to look for boids within a certain field of view of the boids

--- 3D Simulation/Crowd simulation/Assets/Scripts/Boid.cs
+++ 3D Simulation/Crowd simulation/Assets/Scripts/Boid.cs
@@ -4,11 +4,30 @@ using System.Collections;
 public class Boid : MonoBehaviour {
     private static float speed = 4;
     public Vector3 direction;
+    private float viewingDistance = 20;
     
 	// Use this for initialization
 	void Start () {
         direction = Vector3.forward;
 	}
+
+    List<Boid> findBoidsWithinView()
+    {
+        GameObject[] boids = GameObject.FindGameObjectsWithTag("Boid");
+        List<Boid> closeBoids = new List<Boid>();
+
+        foreach(GameObject boid in boids)
+        {
+            float distance = Vector3.Distance(boid.transform.position, this.transform.position);
+            if (distance < this.viewingDistance && distance != 0)
+            {
+                closeBoids.Add(boid.GetComponent<Boid>());
+            }
+        }
+
+        return closeBoids;
+    }
+
     void Update () {
         float step = speed * Time.deltaTime;
         transform.position += this.direction * speed;
