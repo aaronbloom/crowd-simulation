b0b7061d1cf79c4fc34670062d32b81b1e490821
aaronbloom <hiaaronbloom@gmail.com>
2015-11-05 19:11:13 +0000

    CS-19: Added simple camera orbiting around simulation centre point

---
+++ 3D Simulation/Crowd simulation/Assets/Scripts/Camera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+    private EnvironmentManager environmentManager;
+    private Vector3 rotationCenter;
+    private float rotationSpeed;
+
+    // Use this for initialization
+    void Start () {
+        this.environmentManager = EnvironmentManager.Shared();
+        rotationCenter = (this.environmentManager.Bounds / 2) + this.environmentManager.transform.position;
+        rotationSpeed = 20;
+        transform.LookAt(rotationCenter);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(rotationCenter, Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+}
