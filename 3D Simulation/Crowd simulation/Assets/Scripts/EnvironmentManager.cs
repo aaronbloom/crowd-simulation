ccbf9794d057679827149e9c1a818b4b964e0210
aaronbloom <hiaaronbloom@gmail.com>
2015-11-05 14:43:10 +0000

    CS-15: Added sharing of boundaries by making Environment manager available to other game objects. Changed plane logic to accommodate to plane normal direction.

--- 3D Simulation/Crowd simulation/Assets/Scripts/EnvironmentManager.cs
+++ 3D Simulation/Crowd simulation/Assets/Scripts/EnvironmentManager.cs
@@ -3,8 +3,13 @@ using System.Collections;
 
 public class EnvironmentManager : MonoBehaviour {
 
+    private static EnvironmentManager shared;
     public Vector3 Bounds;
-    public Plane[] boundaries;
+    private Plane[] boundaries;
+    public Plane[] Boundaries
+    {
+        get { return boundaries; }
+    }
 
     private Plane[] constructCuboidBoundary (Vector3 bounds, Vector3 origin)
     {
@@ -12,9 +17,9 @@ public class EnvironmentManager : MonoBehaviour {
         boundaries[0] = new Plane(Vector3.up, origin);                  //bottom (ground) plane
         boundaries[1] = new Plane(Vector3.forward, origin);             //front
         boundaries[2] = new Plane(Vector3.right, origin);               //left
-        boundaries[3] = new Plane(Vector3.up, origin + bounds);         //top
-        boundaries[4] = new Plane(Vector3.forward, origin + bounds);    //back
-        boundaries[5] = new Plane(Vector3.right, origin + bounds);      //right
+        boundaries[3] = new Plane(Vector3.down, origin + bounds);       //top
+        boundaries[4] = new Plane(Vector3.back, origin + bounds);       //back
+        boundaries[5] = new Plane(Vector3.left, origin + bounds);       //right
 
         return boundaries;
     }
@@ -28,4 +33,24 @@ public class EnvironmentManager : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    // Setup the static instance of this EnvironmentManager
+    private void Awake()
+    {
+        // Set the 'shared' variable as this environment manager
+        if (shared == null)
+        {
+            shared = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    // Return the static instance of this GameManager
+    public static EnvironmentManager Shared()
+    {
+        return shared;
+    }
 }
