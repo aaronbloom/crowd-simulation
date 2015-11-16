da1839e045c4f7579ca8af10a85da303daf73cd2
James Couzens <james@rcouzens.com>
2015-11-16 14:35:53 +0000

    CS-36: Converted Camera to prefab, Bootstrapper now initialises..

--- 3D Simulation/Crowd simulation/Assets/Scripts/BootStrapper.cs
+++ 3D Simulation/Crowd simulation/Assets/Scripts/BootStrapper.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-class Bootstrapper : MonoBehaviour {
+class BootStrapper : MonoBehaviour {
 
     //System properties
     int boidPopulation = 100;
@@ -8,14 +8,24 @@ class Bootstrapper : MonoBehaviour {
     //System Fields
     private BoidManager boidManager;
     private EnvironmentManager environmentManager;
+    private CameraController cameraController;
 
     void Awake() {
         environmentManager = new EnvironmentManager();
     }
 
     void Start() {
+        initialise("Camera");
         boidManager = new BoidManager(boidPopulation);
         boidManager.SpawnBoids();
+    }
+
+    static void initialise(string prefabName) {
+        MonoBehaviour.Instantiate(Resources.Load("Prefabs/" + prefabName));
+    }
+
+    static void initialise(string prefabName, Vector3 position, Quaternion rotation) {
+        MonoBehaviour.Instantiate(Resources.Load("Prefabs/" + prefabName), position, rotation);
     }
 
 }
