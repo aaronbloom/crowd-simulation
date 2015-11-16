cadce575e09f6f5fc26ac556222c7385881fe78e
Michael Gould <MichaelJGould@live.co.uk>
2015-11-16 17:25:59 +0000

    CS-39: Refactor BoidManager.cs

--- 3D Simulation/Crowd simulation/Assets/Scripts/BoidManager.cs
+++ 3D Simulation/Crowd simulation/Assets/Scripts/BoidManager.cs
@@ -4,28 +4,39 @@ using System.Collections;
 public class BoidManager {   
     
     private const int boidHeight = 2;
+    private static readonly string BoidPrefab = "Prefabs/CylinderBoid";
+
     private int NumberOfBoids;
-    private Vector3 position;
     private Quaternion rotation;
     private EnvironmentManager EnvironmentManager;
 
     public BoidManager(int numOfBoids) {
         EnvironmentManager = EnvironmentManager.Shared();
         NumberOfBoids = numOfBoids;
-        position = Vector3.zero;
         rotation = Quaternion.identity;
     }
 
     public void SpawnBoids() {
         System.Random random = new System.Random();
-
-        for (int i = 0; i < NumberOfBoids; i++) {
-            Vector3 positionOffset = new Vector3(
-                random.Next(0, (int)EnvironmentManager.Bounds.x),
-                boidHeight,
-                random.Next(0, (int)EnvironmentManager.Bounds.z));
-            MonoBehaviour.Instantiate(Resources.Load("Prefabs/CylinderBoid"), position + positionOffset, rotation);
+        for (int i = 0; i < NumberOfBoids; i++)
+        {
+            spawnBoid(random);
         }
     }
 
+    private void spawnBoid(System.Random random)
+    {
+        Vector3 positionOffset = generateRandomPosition(random);
+        Vector3 position = Vector3.zero + positionOffset;
+        Object.Instantiate(Resources.Load(BoidPrefab), position, rotation);
+    }
+
+    private Vector3 generateRandomPosition(System.Random random)
+    {
+        Vector3 environmentBounds = EnvironmentManager.Bounds;
+        int xLimit = (int)environmentBounds.x;
+        int zLimit = (int)environmentBounds.z;
+
+        return new Vector3(random.Next(0, xLimit), boidHeight, random.Next(0, zLimit));
+    }
 }
