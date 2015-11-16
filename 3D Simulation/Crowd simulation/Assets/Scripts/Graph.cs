1376e1bfbd1cc5508751c210990183707e51e901
aaronbloom <hiaaronbloom@gmail.com>
2015-11-16 22:03:18 +0000

    CS-42: Added Graph, Node and Transition classes

---
+++ 3D Simulation/Crowd simulation/Assets/Scripts/Graph.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph {
+
+    public List<Node> Nodes { get; private set; }
+
+    public Graph(List<Node> nodes) {
+        this.Nodes = nodes;
+    }
+
+}
