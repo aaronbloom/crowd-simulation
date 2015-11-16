ae04618c59d774308cbb97c7347ff4a9b005f49c
James Couzens <james@rcouzens.com>
2015-11-16 23:22:26 +0000

    CS-41: Added Path Class to represent Boid route. Static method for A* generation future state. Tweaked Node class to allow for Transition Dictionary

---
+++ 3D Simulation/Crowd simulation/Assets/Scripts/Path.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Path {
+
+    private List<Node> nodes;
+    public Node NextNode {
+        get {
+            return nodes[0];
+        }
+    }
+    public Node GoalNode {
+        get {
+            return nodes[nodes.Count - 1];
+        }
+    }
+
+    public Path(List<Node> nodes) {
+        this.nodes = nodes;
+    }
+
+    
+
+}
+
