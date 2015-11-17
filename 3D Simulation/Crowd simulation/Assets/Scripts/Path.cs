0ade307ff98b67a8a8db784c021ab7a41e2eddad
James Couzens <james@rcouzens.com>
2015-11-17 00:14:55 +0000

    CS-41: Commented Astar method

--- 3D Simulation/Crowd simulation/Assets/Scripts/Path.cs
+++ 3D Simulation/Crowd simulation/Assets/Scripts/Path.cs
@@ -6,17 +6,28 @@ using System.Text;
 class Path {
 
     private List<Node> nodes;
-    public Node NextNode {
+    //Removes the next node, returning it
+    public Node PopNode {
+        get {
+            Node n = PeakNode;
+            nodes.Remove(n);
+            return n;
+        }
+    }
+    //Returns the next node
+    public Node PeakNode {
         get {
             return nodes[0];
         }
     }
+    //Returns the Goal Node
     public Node GoalNode {
         get {
             return nodes[nodes.Count - 1];
         }
     }
 
+    //Constructs Path from ordered node list
     public Path(List<Node> nodes) {
         this.nodes = nodes;
     }
@@ -25,44 +36,65 @@ class Path {
 
         List<Node> closedSet = new List<Node>();
         List<Node> openSet = new List<Node>();
-        //came from?
+
+        //Special case for startNode
         openSet.Add(startNode);
 
+        //Extension fields for Node
         Dictionary<Node, float> fScores = new Dictionary<Node, float>();
         Dictionary<Node, float> gScores = new Dictionary<Node, float>();
         Dictionary<Node, float> hScores = new Dictionary<Node, float>();
-        Dictionary<Node, Node> parents = new Dictionary<Node, Node>();  // <Node,Node> <child,parent>
+
+        // <Node,Node> <child,parent>
+        Dictionary<Node, Node> parents = new Dictionary<Node, Node>();  
+
+        //Special case for startNode
         parents.Add(startNode, null);     
 
+        //Process Open Set
         while (openSet.Count > 0) {
-            Node currentNode = lowestFScoreInOpenSet(fScores, openSet); //needs renaming
-            openSet.Remove(currentNode);
-            foreach (var successorNode in currentNode.TransitionsTo) {
-                if (successorNode == goalNode) {
+            //Choose most promising node
+            Node mostPromisingNode = lowestFScoreInOpenSet(fScores, openSet); //needs renaming
+            //Remove most promising node from open set
+            openSet.Remove(mostPromisingNode);
+            //Process Nodes which most Promising Node connects to
+            foreach (var candidatePromisingNode in mostPromisingNode.TransitionsTo) {
+                //See if we're at our destination
+                if (candidatePromisingNode == goalNode) {#
+                    //return the path to the goal from start
                     return new Path(convertParentageToList(parents, goalNode));
                 }
-                float g = gScores[currentNode] + distanceBetweenNodes(successorNode, currentNode);
-                float h = distanceBetweenNodes(goalNode, successorNode);
-                float f = gScores[successorNode] + hScores[successorNode];
-
-                if (openSet.Contains(successorNode) && fScores[successorNode] < f) {
+                //calculate heuristics for candiate node
+                float g = gScores[mostPromisingNode] + distanceBetweenNodes(candidatePromisingNode, mostPromisingNode);
+                float h = distanceBetweenNodes(goalNode, candidatePromisingNode);
+                float f = gScores[candidatePromisingNode] + hScores[candidatePromisingNode];
+                //check this is the best route we know to the candidate
+                if (openSet.Contains(candidatePromisingNode) && fScores[candidatePromisingNode] < f) {
                     //skip, already better path for this node in open list
-                } else if (closedSet.Contains(successorNode) && fScores[successorNode] < f) {
+                } else if (closedSet.Contains(candidatePromisingNode) && fScores[candidatePromisingNode] < f) {
                     //skip, already better path for this node in closed list
                 } else {
-                    if (!parents.ContainsKey(successorNode)) parents.Remove(successorNode);
-                    parents.Add(successorNode, currentNode);
-                    openSet.Add(successorNode);
-                    gScores[successorNode] = g;
-                    hScores[successorNode] = h;
-                    fScores[successorNode] = f;
+                    //this is the best route to the candidate node
+                    //remove old entries
+                    if (!parents.ContainsKey(candidatePromisingNode)) parents.Remove(candidatePromisingNode);
+                    //add parent for pathing
+                    parents.Add(candidatePromisingNode, mostPromisingNode);
+                    //add to open set
+                    openSet.Add(candidatePromisingNode);
+                    //update stored node heuristics
+                    gScores[candidatePromisingNode] = g;
+                    hScores[candidatePromisingNode] = h;
+                    fScores[candidatePromisingNode] = f;
                 }
             }
-            closedSet.Add(currentNode);
+            //all candidates processed, close node
+            closedSet.Add(mostPromisingNode);
         }
+        //probably should throw exception here.
         return new Path(convertParentageToList(parents, goalNode));
     }
 
+    //Convert Dictionary Linkages to Linear Ordered List
     private static List<Node> convertParentageToList(Dictionary<Node, Node> childParent, Node goal) {
         List<Node> list = new List<Node>();
         list.Add(goal);
@@ -73,10 +105,12 @@ class Path {
         return list;
     }
 
+    //Return linear distance between 2 nodes
     private static float distanceBetweenNodes(Node a, Node b) {
         return (a.Position - b.Position).magnitude;
     }
 
+    //Find the lowest scoring node in the provided set & dictionary.
     private static Node lowestFScoreInOpenSet(Dictionary<Node, float> scores, List<Node> set) {
         float lowest = float.MaxValue;
         Node winningNode = null;
