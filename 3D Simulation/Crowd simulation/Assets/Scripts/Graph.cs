358ec6cf084c7785159b11174f4092e776dce24c
aaronbloom <hiaaronbloom@gmail.com>
2015-11-17 00:09:06 +0000

    CS-40: Added graph nodes per meter (node precision)

--- 3D Simulation/Crowd simulation/Assets/Scripts/Graph.cs
+++ 3D Simulation/Crowd simulation/Assets/Scripts/Graph.cs
@@ -9,28 +9,29 @@ public class Graph {
         this.Nodes = nodes;
     }
 
-    public static Graph ConstructGraph(Environment environment) {
+    public static Graph ConstructGraph(Environment environment, float nodesPerMeter) {
         Vector2 dimensions = environment.GetFloorDimentions();
-        int graphWidthInNodes = (int) dimensions.x;
-        int graphHeightInNodes = (int) dimensions.y;
+        int graphWidthInNodes = (int) (dimensions.x * nodesPerMeter);
+        int graphHeightInNodes = (int) (dimensions.y * nodesPerMeter);
 
-        List<Node> nodes = generateLatticeGraph(graphWidthInNodes, graphHeightInNodes);
+        List<Node> nodes = generateLatticeGraph(graphWidthInNodes, graphHeightInNodes, nodesPerMeter);
 
         return new Graph(nodes);
     }
 
-    private static List<Node> generateNodeGraph(int width, int height) {
+    private static List<Node> generateNodeGraph(int width, int height, float nodesPerMeter) {
         List<Node> nodes = new List<Node>();
         for (int a = 0; a < width; a++) {
             for (int b = 0; b < height; b++) {
-                nodes.Add(new Node(new Vector3(a, 0, b)));
+                Vector3 position = new Vector3(a / nodesPerMeter, 0, b / nodesPerMeter);
+                nodes.Add(new Node(position));
             }
         }
         return nodes;
     }
 
-    private static List<Node> generateLatticeGraph(int width, int height) {
-        List<Node> nodes = generateNodeGraph(width, height);
+    private static List<Node> generateLatticeGraph(int width, int height, float nodesPerMeter) {
+        List<Node> nodes = generateNodeGraph(width, height, nodesPerMeter);
 
         //only need to look right and down per node to constuct full lattice
         for (int a = 0; a < width; a++) {
