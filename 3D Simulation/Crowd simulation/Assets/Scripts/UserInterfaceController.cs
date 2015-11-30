392214317b757e8f2a56a87fde9f959268eb7cd8
aaronbloom <hiaaronbloom@gmail.com>
2015-11-30 20:28:54 +0000

    CS-51: Added new user environment builder menu. When in build mode a new user environment builder is created

--- 3D Simulation/Crowd simulation/Assets/Scripts/UserInterfaceController.cs
+++ 3D Simulation/Crowd simulation/Assets/Scripts/UserInterfaceController.cs
@@ -4,16 +4,20 @@ public class UserInterfaceController : MonoBehaviour {
 
     private GameObject mainMenu;
     private GameObject setupMenu;
+    private GameObject environmentBuilderMenu;
     private BootStrapper bootStrapper;
+    private Object userEnvironmentBuilder;
 
     void Awake() {
         mainMenu = GameObject.Find("MainMenu");
         setupMenu = GameObject.Find("SetupMenu");
+        environmentBuilderMenu = GameObject.Find("EnvironmentBuilderMenu");
     }
 
 	void Start () {
 	    mainMenu.SetActive(true);
         setupMenu.SetActive(false);
+        environmentBuilderMenu.SetActive(false);
         bootStrapper = GameObject.Find("Bootstrapper").GetComponent<BootStrapper>();
     }
 	
@@ -26,9 +30,17 @@ public class UserInterfaceController : MonoBehaviour {
         setupMenu.SetActive(true);
     }
 
+    public void StartEnvironmentBuilder() {
+        setupMenu.SetActive(false);
+        environmentBuilderMenu.SetActive(true);
+        userEnvironmentBuilder = MonoBehaviour.Instantiate(Resources.Load("Prefabs/UserEnvironmentBuilder"));
+    }
+
     public void StartSimulation() {
+        GameObject.Destroy(userEnvironmentBuilder);
         mainMenu.SetActive(false);
         setupMenu.SetActive(false);
+        environmentBuilderMenu.SetActive(false);
         int numberOfBoids = setupMenu.GetComponent<SliderController>().Value;
         bootStrapper.StartSimulation(numberOfBoids);
     }
