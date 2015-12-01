73ff773115ef2e1616ee68fad98fb5e938c232c6
aaronbloom <hiaaronbloom@gmail.com>
2015-12-01 01:07:57 +0000

    CS-52: Added basic wall cursor, with ghosting. Snaps to grid as to give a preview to the user as where block placement will be

--- 3D Simulation/Crowd simulation/Assets/Scripts/UserEnvironmentBuilder.cs
+++ 3D Simulation/Crowd simulation/Assets/Scripts/UserEnvironmentBuilder.cs
@@ -3,33 +3,50 @@ using System.Collections;
 
 public class UserEnvironmentBuilder : MonoBehaviour {
 
-    void Update() {
-        if (Input.GetMouseButtonDown(0)) { //left clicked
-            Plane plane = new Plane(Vector3.up, Vector3.zero); // floor
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    private Object ghostedItemCursor;
+    private const float wallSize = 4;
 
-            float distance;
-            if (plane.Raycast(ray, out distance)) {
-                Vector3 groundPoint = ray.GetPoint(distance);
-                PlaceWall(groundPoint);
-            }
+    void Start() {
+        ghostedItemCursor = MonoBehaviour.Instantiate(Resources.Load("Prefabs/WallCursor"));
+    }
+
+    void Update() {
+        UpdateCursorPosition();
+
+        if (Input.GetMouseButtonDown(0)) { //left mouse clicked
+            PlaceWall(MousePositionToGroundPosition());
         }
     }
 
-    private Vector3 PositionToGridPosition(Vector3 position, float objectSize) {
-        Vector3 gridPosition = position;
+    private static Vector3 MousePositionToGroundPosition() {
+        Plane plane = new Plane(Vector3.up, Vector3.zero); // floor
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        float distance;
+        if (plane.Raycast(ray, out distance)) {
+            return ray.GetPoint(distance);
+        }
+        return Vector3.zero;
+    }
+
+    private void UpdateCursorPosition() {
+        ((GameObject) ghostedItemCursor).transform.position = PositionToGridPosition(MousePositionToGroundPosition(), wallSize);
+
+    }
+
+    private static Vector3 PositionToGridPosition(Vector3 position, float objectSize) {
+        var gridPosition = position;
         gridPosition -= Vector3.one * (objectSize / 2);
         gridPosition /= objectSize;
         gridPosition = new Vector3(Mathf.Round(gridPosition.x), Mathf.Round(gridPosition.y), Mathf.Round(gridPosition.z));
         gridPosition *= objectSize;
         gridPosition += Vector3.one * (objectSize / 2);
+        gridPosition.y = objectSize / 2; // so it sits at ground level
         return gridPosition;
     }
 
-    private void PlaceWall(Vector3 position) {
-        float wallSize = 4;
-        Vector3 wallLocation = PositionToGridPosition(position, wallSize);
-        wallLocation.y = wallSize / 2;
+    private static void PlaceWall(Vector3 position) {
+        var wallLocation = PositionToGridPosition(position, wallSize);
         MonoBehaviour.Instantiate(Resources.Load("Prefabs/Wall"), wallLocation, Quaternion.identity);
     }
 }
