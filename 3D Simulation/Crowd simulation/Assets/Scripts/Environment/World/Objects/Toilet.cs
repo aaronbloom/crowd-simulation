9734956440e892931b4a0bd53f554bf799aa08b6
Michael Gould <MichaelJGould@live.co.uk>
2015-12-11 22:12:35 +0000

    CS-98: Create Toilet WorldObject

---
+++ 3D Simulation/Crowd simulation/Assets/Scripts/Environment/World/Objects/Toilet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Environment.World.Objects {
+    public class Toilet : WorldObject, Collidable {
+
+        public Toilet() : base() {
+            this.Identifier = "Toilet";
+            this.InitialRotationOffSet = Quaternion.Euler(-90, 0, 180);
+            this.Size = 4;
+        }
+
+        public WorldObject getObject() {
+            return this;
+        }
+    }
+}
