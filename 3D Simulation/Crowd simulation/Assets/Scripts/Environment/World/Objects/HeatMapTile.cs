edd6afa9527012d017f406ee341e6da318a41775
aaronbloom <hiaaronbloom@gmail.com>
2016-02-02 18:39:04 +0000

    CS-101: Added heat map tile prefabs and world entity classes

---
+++ 3D Simulation/Crowd simulation/Assets/Scripts/Environment/World/Objects/HeatMapTile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Environment.World.Objects {
+    public class HeatMapTile : WorldObject {
+
+        public static int TileSize = 4;
+
+        public HeatMapTile() : base() {
+            this.Identifier = "HeatMapTile";
+            this.InitialRotationOffSet = Quaternion.Euler(90, 0, 0);
+            this.InitialPositionOffSet = new Vector3(0, 0.5f, 0);
+            this.Size = TileSize;
+        }
+
+    }
+}
