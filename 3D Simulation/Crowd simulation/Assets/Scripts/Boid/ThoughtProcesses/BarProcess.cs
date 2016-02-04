eb33baed578c7da8e81d0536c46672696f5a2b21
James Couzens <james@rcouzens.com>
2016-02-04 18:06:17 +0000

    CS-82: Implemented DanceProcess and BarProcess

--- 3D Simulation/Crowd simulation/Assets/Scripts/Boid/ThoughtProcesses/BarProcess.cs
+++ 3D Simulation/Crowd simulation/Assets/Scripts/Boid/ThoughtProcesses/BarProcess.cs
@@ -7,5 +7,37 @@ namespace Assets.Scripts.Boid.ThoughtProcesses
 {
     class BarProcess : ThoughtProcess
     {
+        private Boid owner;
+        private Need ownerDesire;
+
+        public BarProcess(Boid boid, Need toSatisfy) : base()
+        {
+            owner = boid;
+            processList.Add((Action)navigateToBar);
+            processList.Add((Action)continueWalkToBar);
+            processList.Add((Action)reachBar);
+        }
+
+        private void navigateToBar()
+        {
+            GoalSeekingBehaviour gsb = new GoalSeekingBehaviour(owner, 10f, 2.5f); //fekin magic numbers, thanks Aaron
+            gsb.ChooseClosestFromList(BootStrapper.EnvironmentManager.CurrentEnvironment.World.Bars);
+            owner.behaviour = gsb;
+            NextStep();
+        }
+
+        private void continueWalkToBar()
+        {
+            if (owner.behaviour.BehaviourComplete)
+            {
+                NextStep();
+            }
+        }
+
+        private void reachBar()
+        {
+            ownerDesire.Satisfy();
+            NextStep();
+        }
     }
 }
--- 3D Simulation/Crowd simulation/Assets/Scripts/Boid/ThoughtProcesses/DanceProcess.cs
+++ 3D Simulation/Crowd simulation/Assets/Scripts/Boid/ThoughtProcesses/DanceProcess.cs
@@ -7,5 +7,39 @@ namespace Assets.Scripts.Boid.ThoughtProcesses
 {
     class DanceProcess : ThoughtProcess
     {
+        private Boid owner;
+        private Need ownerDesire;
+
+        public DanceProcess(Boid boid, Need toSatisfy) : base()
+        {
+            owner = boid;
+            processList.Add((Action) navigateToStage);
+            processList.Add((Action) continueWalkToStage);
+            processList.Add((Action) reachStage);
+        }
+
+        private void navigateToStage()
+        {
+            GoalSeekingBehaviour gsb = new GoalSeekingBehaviour(owner, 10f, 2.5f); //fekin magic numbers, thanks Aaron
+            gsb.ChooseClosestFromList(BootStrapper.EnvironmentManager.CurrentEnvironment.World.Stages);
+            owner.behaviour = gsb;
+            NextStep();
+        }
+
+        private void continueWalkToStage()
+        {
+            if (owner.behaviour.BehaviourComplete)
+            {
+                NextStep();
+            }
+        }
+
+        private void reachStage()
+        {
+            //probably change this to some loiter behaviour
+            //satisfy slowly
+            ownerDesire.Satisfy();
+            NextStep();
+        }
     }
 }
