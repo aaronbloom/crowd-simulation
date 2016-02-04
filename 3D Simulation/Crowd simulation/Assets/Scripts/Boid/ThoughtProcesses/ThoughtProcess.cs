8cf57f2fbee5f9cf5a5084386a55df973de09a62
James Couzens <james@rcouzens.com>
2016-02-04 15:25:19 +0000

    CS-82: Added Processes

---
+++ 3D Simulation/Crowd simulation/Assets/Scripts/Boid/ThoughtProcesses/ThoughtProcess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Boid
+{
+    class ThoughtProcess
+    {
+        private int processStep;
+        private List<object> processList;
+
+        public void NextStep()
+        {
+            processStep++;
+        }
+
+        public void RunCurrentProcess()
+        {
+            callProcess(processStep);
+        }
+
+        private void callProcess(int step)
+        {
+            ((Action) processList[step])();
+        }
+    }
+}
