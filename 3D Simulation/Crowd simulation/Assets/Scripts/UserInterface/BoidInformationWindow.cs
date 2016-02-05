95ac9e38811f6358613c85bad9c4549ddb75fcac
aaronbloom <hiaaronbloom@gmail.com>
2016-02-05 15:18:48 +0000

    CS-109: Added "plumb-bob" style boid selection highlighting

--- 3D Simulation/Crowd simulation/Assets/Scripts/UserInterface/BoidInformationWindow.cs
+++ 3D Simulation/Crowd simulation/Assets/Scripts/UserInterface/BoidInformationWindow.cs
@@ -6,6 +6,7 @@ namespace Assets.Scripts.UserInterface {
         private readonly GameObject boidInformationWindow;
         private Boid.Boid currentBoid;
         private const string BoidInformationWindowName = "BoidInformationWindow";
+        private GameObject selectionGameObject;
 
         public BoidInformationWindow() {
             boidInformationWindow = GameObject.Find(BoidInformationWindowName);
@@ -21,6 +22,8 @@ namespace Assets.Scripts.UserInterface {
                     currentBoid = raycastHit.transform.GetComponent<Boid.Boid>();
                 } else {
                     currentBoid = null;
+                    Object.Destroy(selectionGameObject);
+                    selectionGameObject = null;
                     UserInterfaceController.HideMenu(boidInformationWindow);
                 }
             }
@@ -28,11 +31,21 @@ namespace Assets.Scripts.UserInterface {
 
         public void Update() {
             if (currentBoid != null) {
+                UpdateSelectionGameObject();
                 GameObject.Find("ThirstText").GetComponent<Text>().text = "Thirst: " + currentBoid.Thirst;
                 GameObject.Find("ToiletNeedText").GetComponent<Text>().text = "Toilet need: " + currentBoid.ToiletNeed;
                 GameObject.Find("DanceNeedText").GetComponent<Text>().text = "Dance need: " + currentBoid.DanceNeed;
             }
         }
 
+        private void UpdateSelectionGameObject() {
+            if (selectionGameObject == null) {
+                selectionGameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            }
+            Vector3 currentBoidPosition = currentBoid.transform.position;
+            selectionGameObject.transform.position = currentBoidPosition + new Vector3(0, 4f, 0);
+            selectionGameObject.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+            selectionGameObject.transform.Rotate(Vector3.left + Vector3.down, 100 * Time.deltaTime);
+        }
     }
 }
