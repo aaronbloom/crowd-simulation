9e8b1c9487565001c67cf1f27e9fabee74c7a8d8
aaronbloom <hiaaronbloom@gmail.com>
2016-02-11 19:12:10 +0000

    CS-134: Removed collidable from stage, as it causes problem with navigation

--- 3D Simulation/Crowd simulation/Assets/Scripts/Environment/World/Objects/Stage.cs
+++ 3D Simulation/Crowd simulation/Assets/Scripts/Environment/World/Objects/Stage.cs
@@ -1,17 +1,13 @@
 ﻿using UnityEngine;
 
 namespace Assets.Scripts.Environment.World.Objects {
-    public class Stage : Goal, Collidable {
+    public class Stage : Goal {
 
         public Stage() : base() {
             this.Identifier = "Stage";
             this.InitialRotationOffSet = Quaternion.Euler(0, 0, 0);
             this.InitialPositionOffSet = new Vector3(0, 0, 0);
             this.Size = new Vector3(4, 2, 4);
-        }
-
-        public WorldObject getObject() {
-            return this;
         }
     }
 }
