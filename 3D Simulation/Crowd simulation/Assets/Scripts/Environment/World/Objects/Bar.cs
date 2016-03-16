371074292f7bc9ee943031b4823bd8788ed798e4
Aaron Bloom <hiaaronbloom@gmail.com>
2016-03-16 23:57:56 +0000

    CS-198: Boids can no longer enter the bar, and will navigate around them

--- 3D Simulation/Crowd simulation/Assets/Scripts/Environment/World/Objects/Bar.cs
+++ 3D Simulation/Crowd simulation/Assets/Scripts/Environment/World/Objects/Bar.cs
@@ -14,7 +14,7 @@ namespace Assets.Scripts.Environment.World.Objects {
         public Bar() : base() {
             this.Identifier = IdentifierStatic;
             this.InitialRotationOffSet = Quaternion.Euler(90, 0, 0);
-            this.InitialPositionOffSet = new Vector3(0, 0.01f, 0);
+            this.InitialPositionOffSet = new Vector3(0, 0, 0);
             this.Size = SizeStatic;
             this.GridPlaceable = false;
         }
