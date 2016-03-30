34aaff4de487105d59149afb1c46d0c852b17f7b
James Couzens <james@rcouzens.com>
2016-03-30 19:36:23 +0100

    Various James Classes XMLCOD'd

--- 3D Simulation/Crowd simulation/Assets/Scripts/Boid/NameGenerator.cs
+++ 3D Simulation/Crowd simulation/Assets/Scripts/Boid/NameGenerator.cs
@@ -15,6 +15,11 @@ namespace Assets.Scripts.Boid {
         private static readonly List<string> generatedNames = new List<string>();
         private static bool listsLoaded;
 
+        /// <summary>
+        /// Generates a name that's not guarenteed to be unique, but most likely will be. (3 attempts are made at unique gen)
+        /// </summary>
+        /// <param name="gender">The required gender of the name</param>
+        /// <returns>name</returns>
         public static string GenerateFairlyUniqueName(Gender gender) {
             string potentialName = GenerateName(gender);
             for (int i = 0; (i < 3) && !generatedNames.Contains(potentialName); i++) {
@@ -24,10 +29,20 @@ namespace Assets.Scripts.Boid {
             return potentialName;
         }
 
+        /// <summary>
+        /// Generates a name that's not guarenteed to be unique
+        /// </summary>
+        /// <param name="gender">The required gender of the name</param>
+        /// <returns>name</returns>
         public static string GenerateName(Gender gender) {
             return GenerateFirstName(gender) + " " + GenerateSecondName();
         }
 
+        /// <summary>
+        /// Generates a first name that's not guarenteed to be unique
+        /// </summary>
+        /// <param name="gender">The required gender of the name</param>
+        /// <returns>name</returns>
         public static string GenerateFirstName(Gender gender) {
             if(!listsLoaded) LoadLists();
             return gender == Gender.Male ?
@@ -35,11 +50,19 @@ namespace Assets.Scripts.Boid {
                 femaleFirstNames[random.Next(0, femaleFirstNames.Count)];
         }
 
+        /// <summary>
+        /// Generates a second name that's not guarenteed to be unique
+        /// </summary>
+        /// <param name="gender">The required gender of the name</param>
+        /// <returns>name</returns>
         public static string GenerateSecondName() {
             if (!listsLoaded) LoadLists();
             return secondNames[random.Next(0, secondNames.Count)];
         }
 
+        /// <summary>
+        /// Loads the name files into string lists
+        /// </summary>
         public static void LoadLists() {
             maleFirstNames = loadList("MaleFirstNames");
             femaleFirstNames = loadList("FemaleFirstNames");
@@ -47,6 +70,11 @@ namespace Assets.Scripts.Boid {
             listsLoaded = true;
         }
 
+        /// <summary>
+        /// Opens a file with name: <paramref name="fileName"/> and turns each line into a string
+        /// </summary>
+        /// <param name="fileName">the name of the file to be loaded</param>
+        /// <returns>List of strings representing lines in the file</returns>
         private static List<string> loadList(string fileName) {
             List<string> l = new List<string>();
             try {
--- 3D Simulation/Crowd simulation/Assets/Scripts/Environment/World/World.cs
+++ 3D Simulation/Crowd simulation/Assets/Scripts/Environment/World/World.cs
@@ -4,6 +4,10 @@ using Assets.Scripts.Environment.World.Objects;
 using UnityEngine;
 
 namespace Assets.Scripts.Environment.World {
+
+    /// <summary>
+    /// The purpose of World is to store all the world objects within a particular Environment, and allow you to query them
+    /// </summary>
     public class World {
 
         public List<WorldObject> Objects { get; private set; }
@@ -18,15 +22,26 @@ namespace Assets.Scripts.Environment.World {
         public List<Bar> Bars;
         public List<ICollidable> Collidables;
 
+        /// <summary>
+        /// Creates new World object
+        /// </summary>
         public World() {
             Objects = new List<WorldObject>();
         }
-
+        
+        /// <summary>
+        /// Returns all objects of type <typeparamref name="T"/> which are stored in this World
+        /// </summary>
+        /// <typeparam name="T">The type of world object you wish to return</typeparam>
+        /// <returns>The objects within world of type <typeparamref name="T"/></returns>
         public List<T> ObjectSublist<T>() where T : WorldObject {
             return Objects.OfType<T>().ToList();
         }
 
-        //MUST be called if list contents are changed!
+        /// <summary>
+        /// Updates all the cached sublists so queries of this class are executed faster.
+        /// </summary>
+        /// <remarks>MUST be called if list contents are changed!</remarks>
         public void updateSublists() {
             Goals = ObjectSublist<Goal>();
             Entrances = ObjectSublist<Entrance>();
@@ -39,6 +54,12 @@ namespace Assets.Scripts.Environment.World {
             Collidables = Objects.OfType<ICollidable>().ToList();
         }
 
+        /// <summary>
+        /// Adds a WorldObject to the world
+        /// </summary>
+        /// <remarks>Updates sublists too</remarks>
+        /// <param name="worldObject">The WorldObject to be added</param>
+        /// <returns>true if the object could be added, false if there was already an object at it's position</returns>
         public bool AddObject(WorldObject worldObject) {
             bool alreadyOccupied = AlreadyOccupied(worldObject);
             if (!alreadyOccupied) {
@@ -50,6 +71,12 @@ namespace Assets.Scripts.Environment.World {
             return !alreadyOccupied;
         }
 
+        /// <summary>
+        /// Removes the WorldObject from the world
+        /// <remarks>Updates sublists too</remarks>
+        /// </summary>
+        /// <param name="worldObject">Object to remove</param>
+        /// <returns>if the object was successfully removed</returns>
         public bool RemoveObject(WorldObject worldObject) {
             bool success = Objects.Remove(worldObject);
             if (success) {
@@ -59,20 +86,38 @@ namespace Assets.Scripts.Environment.World {
             return success;
         }
 
+        /// <summary>
+        /// Sees if there's a WorldObject occupying the position: <paramref name="location"/>
+        /// </summary>
+        /// <param name="location">The position to check</param>
+        /// <returns>true if there's already a WorldObject at this point</returns>
         public bool PointAlreadyOccupied(Vector3 location) {
             return Objects.Any(worldObject => worldObject.SamePosition(location));
         }
 
+        /// <summary>
+        /// Sees if there's a WorldObject occupying the area: <paramref name="location"/>
+        /// </summary>
+        /// <param name="location">The position to check</param>
+        /// <returns>true if there's already a WorldObject in this area</returns>
         public bool SpaceAlreadyOccupied(Vector3 location)
         {
             return Objects.Any(worldObject => worldObject.WithinBounds(location));
         }
 
-
+        /// <summary>
+        /// Sees if there's a WorldObject occupying the area of WorldObject: <paramref name="worldObject"/>
+        /// </summary>
+        /// <param name="location">The position to check</param>
+        /// <returns>true if there's already a WorldObject in this area</returns>
         public bool AlreadyOccupied(WorldObject worldObject) {
             return Objects.Any(otherWorldObject => otherWorldObject.WithinBounds(worldObject));
         }
 
+        /// <summary>
+        /// Checks if the world has one of every WorldObject
+        /// </summary>
+        /// <returns>True if world contains every type of WorldObject</returns>
         public bool IsValidWorld() {
             return (Entrances.Count > 0) &&
                    (MaleToilets.Count > 0) &&
